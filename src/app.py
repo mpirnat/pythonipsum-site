c4604d6479b09c2a5a54fb93ea43cbb0015bfaca
Mike Pirnat <mpirnat@gmail.com>
2013-03-16 16:14:45 -0700

    Added a RESTish JSON API

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, g, request, session, abort, \
-        redirect, render_template, url_for
+        redirect, render_template, url_for, jsonify
 from pythonipsum import get_paragraphs
 
 import forms
@@ -23,7 +23,22 @@ def homepage():
 
     paragraph_options = range(1, 11)
 
+    api_url = url_for('api')
+
     return render_template('index.html', **locals())
+
+
+@app.route('/api')
+def api():
+    form = forms.LoremIpsumForm(request.form or request.args)
+    if not form.validate():
+        abort(400)
+
+    num_paragraphs = form.num_paragraphs.data or 1
+    paragraphs = get_paragraphs(num_paragraphs)
+
+    return jsonify(paragraphs=paragraphs)
+
 
 if __name__ == '__main__':
     app.run()
